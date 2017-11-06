9de809b49eaed19679c9f9dcb5f77c68aa9711bd
Pekka Ylenius <pekka@ylenius.net>
2017-11-06 15:18:06 +0200

    Added support for product options.

--- upload/admin/controller/module/nettivarasto.php
+++ upload/admin/controller/module/nettivarasto.php
@@ -7,8 +7,7 @@ class ControllerModuleNettivarasto extends Controller {
 	private $api = '';
 	private $order_id = '';
 	
-	public function __construct($registry)
-	{
+	public function __construct($registry){
 		parent::__construct( $registry );
 		$this->error  = array();
 		$this->logger = new Log('MySite.log');
@@ -16,9 +15,11 @@ class ControllerModuleNettivarasto extends Controller {
 		$this->secretToken = $this->config->get('nettivarasto_secret_token');
 		$this->api = new NettivarastoAPI($this->merchantID, $this->secretToken);
   	}
+	
 	public function set_order_id($order_id){
 		$this->order_id = $order_id;
 	}
+	
 	public function index() {
 		$this->load->language('module/nettivarasto');
 
@@ -72,7 +73,6 @@ class ControllerModuleNettivarasto extends Controller {
 		);
 
 		$data['action'] = $this->url->link('module/nettivarasto', 'token=' . $this->session->data['token'], true);
-
 		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true);
 		
 		$this->load->model('extension/extension');
@@ -111,15 +111,13 @@ class ControllerModuleNettivarasto extends Controller {
 			$data['nettivarasto_merchentid'] = $this->request->post['nettivarasto_merchentid'];
 		} else {
 			$data['nettivarasto_merchentid'] = $this->config->get('nettivarasto_merchentid');
-		}
-		
+		}		
 		
 		if (isset($this->request->post['nettivarasto_secret_token'])) {
 			$data['nettivarasto_secret_token'] = $this->request->post['nettivarasto_secret_token'];
 		} else {
 			$data['nettivarasto_secret_token'] = $this->config->get('nettivarasto_secret_token');
 		}
-
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
@@ -128,8 +126,8 @@ class ControllerModuleNettivarasto extends Controller {
 		$data['latest_update_url'] = $this->url->link('module/nettivarasto/get_latest_changes', 'token=' . $this->session->data['token'], true);
 		$this->response->setOutput($this->load->view('module/nettivarasto.tpl', $data));
 	}
-	public function export_product(){
 	
+	public function export_product(){
 		$this->load->language('module/nettivarasto');
 
 		$this->document->setTitle($this->language->get('heading_title'));
@@ -164,7 +162,7 @@ class ControllerModuleNettivarasto extends Controller {
 
 		foreach($shipping_methods as $key=>$value){
 			$strCodeKey	= "nettivarasto_code_".$value;
-			$strStatusKey	= "nettivarasto_status_".$value;
+			$strStatusKey = "nettivarasto_status_".$value;
 
 			$data['shipping_methods'][$key]['code'] = $value;
 			if (isset($this->request->post[$strCodeKey])) {
@@ -203,7 +201,6 @@ class ControllerModuleNettivarasto extends Controller {
 		);
 
 		$data['action'] = $this->url->link('module/nettivarasto', 'token=' . $this->session->data['token'], true);
-
 		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true);
 
 		if (isset($this->request->post['nettivarasto_admin'])) {
@@ -236,39 +233,92 @@ class ControllerModuleNettivarasto extends Controller {
 			$data['nettivarasto_secret_token'] = $this->config->get('nettivarasto_secret_token');
 		}
 
-
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
 		$this->load->model('catalog/product');
 		$results = $this->model_catalog_product->getProducts();
-	
-		$this->load->model('tool/image');
+
+		$this->load->model('tool/image');		
+
+		// export for options
+		$this->load->model('extension/nettivarasto');
+
 		foreach($results as $products){
+			// export for options
+			foreach ($this->model_extension_nettivarasto->getProductOptionsWithValues($products['product_id']) as $option) {
+				$product_option_value_data = array();				
+				
+				foreach ($option['product_option_value'] as $option_value) {
+					if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
+						if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
+							//$price = $this->currency->format($this->tax->calculate($option_value['price'], $product_info['tax_class_id'], $this->config->get('config_tax') ? 'P' : false));
+
+							$pricePrefix = $option_value['price_prefix'];
+							$price = $pricePrefix.$option_value['price'];
+							$optionPrice = ($products['price']+$price);
+						} else {
+							$price = $products['price'];
+						}
+
+						if($option_value['image']){
+							$pictureImage = $this->model_tool_image->resize($option_value['image'], '100','100');
+						} else {
+							$pictureImage = $this->model_tool_image->resize($products['image'],'100','100');
+						}
+						
+						$sku = $products['sku'].'-'.$option_value['name'];
+						$prodName = htmlspecialchars_decode($products['name']).'-'.$option_value['name'];
+
+						$product_option_value_data = array(
+							'Code'        => $sku,
+							'Name'        => $prodName,							
+							'Description' => $products['description'],
+							'EANCode'     => $products['ean'],
+							'InfoUrl'     => $this->url->link('product/product', 'product_id=' . $products['product_id']),
+							'PictureUrl'  => $pictureImage,
+							'SalesPrice'  => $optionPrice,
+							'Currency'    => $this->config->get('config_currency'),
+							'OptionName'  => $option['name'],
+							'Prefix'      => $option_value['price_prefix']
+						);
+						$NV_productsOptions['Products']['Product'][] = $product_option_value_data;						
+					}
+				}				
+			}
+			
 			$product_array = array(
-					'Code' => $products['sku'],
-					'Name' => htmlspecialchars_decode($products['name']),
-					'Description' => $products['description'],
-					'EANCode' => $products['ean'],
-					'InfoUrl' => $this->url->link('product/product', 'product_id=' . $products['product_id']),
-					'PictureUrl'=> $this->model_tool_image->resize($products['image'],'100','100'),
-					'SalesPrice' => $products['price'],
-					'Currency' => $this->config->get('config_currency')
-				  );
-			$NV_products['Products']['Product'][] = $product_array;	  
-		}	
-		$response = $this->api->updateAllProducts($NV_products);
-		if ( $response ) {
-		  if ( ! ( (string)$response['Response']['Info']['@Success'] == 'true' ) ) {
-			 $data['error_warning'] = $response['Response']['Info']['@Error'];
-		  } else {
-			$data['success'] = 'Product export completed.';
-		  }
+				'Code' => $products['sku'],
+				'Name' => htmlspecialchars_decode($products['name']),
+				'Description' => $products['description'],
+				'EANCode' => $products['ean'],
+				'InfoUrl' => $this->url->link('product/product', 'product_id=' . $products['product_id']),
+				'PictureUrl'=> $this->model_tool_image->resize($products['image'],'100','100'),
+				'SalesPrice' => $products['price'],
+				'Currency' => $this->config->get('config_currency')
+			);
+			$NV_products['Products']['Product'][] = $product_array;
 		}
-	$data['product_export_url'] = $this->url->link('module/nettivarasto/export_product', 'token=' . $this->session->data['token'], true);	
-	$data['latest_update_url'] = $this->url->link('module/nettivarasto/get_latest_changes', 'token=' . $this->session->data['token'], true);
-	$this->response->setOutput($this->load->view('module/nettivarasto.tpl', $data));
+		//print '<pre>';
+		//print_r($NV_products);
+		//print_r($NV_productsOptions);
+		//exit;
+		
+		$response = $this->api->updateAllProducts($NV_products);
+		// export for options
+		$responseNew = $this->api->updateAllProducts($NV_productsOptions);
+		
+		if ( $response ) {			
+			if ( ! ( (string)$response['Response']['Info']['@Success'] == 'true' ) ) {
+				$data['error_warning'] = $response['Response']['Info']['@Error'];
+			} else {
+				$data['success'] = 'Product export completed.';
+			}
+		}
+		$data['product_export_url'] = $this->url->link('module/nettivarasto/export_product', 'token=' . $this->session->data['token'], true);	
+		$data['latest_update_url'] = $this->url->link('module/nettivarasto/get_latest_changes', 'token=' . $this->session->data['token'], true);
+		$this->response->setOutput($this->load->view('module/nettivarasto.tpl', $data));
 	}
 	
 	function get_latest_changes() {
@@ -325,7 +375,6 @@ class ControllerModuleNettivarasto extends Controller {
 		);
 
 		$data['action'] = $this->url->link('module/nettivarasto', 'token=' . $this->session->data['token'], true);
-
 		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true);
 
 		if (isset($this->request->post['nettivarasto_admin'])) {
@@ -385,8 +434,8 @@ class ControllerModuleNettivarasto extends Controller {
 		$this->load->model('extension/nettivarasto');
 		if($latestOrders) {
 			foreach($latestOrders as $latestOrder) {
-				$strOrderDetails    = array();
-				$strOrderDetails	=	$this->model_sale_order->getOrder($latestOrder->getReference());
+				$strOrderDetails = array();
+				$strOrderDetails = $this->model_sale_order->getOrder($latestOrder->getReference());
 				switch ( $latestOrder->getStatus() ) {	
 					 case  'SHIPPED': 
 						$this->model_extension_nettivarasto->addHistory($strOrderDetails['order_id'],'5','Nettivarasto change of status to SHIPPED. ');
@@ -422,56 +471,70 @@ class ControllerModuleNettivarasto extends Controller {
 				}
 			 }
 		}
-	$data['success']="Product and order data updated from Nettivarasto.";	
-	$data['product_export_url'] = $this->url->link('module/nettivarasto/export_product', 'token=' . $this->session->data['token'], true);
-	$data['latest_update_url'] = $this->url->link('module/nettivarasto/get_latest_changes', 'token=' . $this->session->data['token'], true);
-	$this->response->setOutput($this->load->view('module/nettivarasto.tpl', $data));
+		$data['success']="Product and order data updated from Nettivarasto.";	
+		$data['product_export_url'] = $this->url->link('module/nettivarasto/export_product', 'token=' . $this->session->data['token'], true);
+		$data['latest_update_url'] = $this->url->link('module/nettivarasto/get_latest_changes', 'token=' . $this->session->data['token'], true);
+		$this->response->setOutput($this->load->view('module/nettivarasto.tpl', $data));
 	}
 	
 	function save_order_to_nettivarasto($order_id = '') {
 		if(isset($this->session->data['order_id'])){
 			$order_id = $this->session->data['order_id'];
-		}elseif(isset($this->request->get['order_id'])){
+		} elseif(isset($this->request->get['order_id'])){
 			$order_id = $this->request->get['order_id'];
 		}
 		$this->load->model('sale/order');
 		$this->load->model('extension/nettivarasto');
 		$order = new NettivarastoAPI_Order($this->api,$order_id);
-		$strOrderDetails	=	$this->model_sale_order->getOrder($order_id);
-	
+		$strOrderDetails = $this->model_sale_order->getOrder($order_id);
+
 		$strShippingCode = trim(strstr($strOrderDetails['shipping_code'],"."),".");
-		
+
 		if($this->config->get("nettivarasto_status_".$strShippingCode)==1){
-				$nettivarasto_shipping_method=$this->config->get("nettivarasto_code_".$strShippingCode);
-				$products	=	$this->model_sale_order->getOrderProducts($order_id);
-				$index=0;
-				foreach($products as $item) {
-				  $order->setOrderLineCode( $index, ($this->model_extension_nettivarasto->getSKUByProductId($item['product_id'])) );
-				  $order->setOrderLineQuantity( $index, ($item['quantity']));
-				  $index++;
-				}
+			$nettivarasto_shipping_method=$this->config->get("nettivarasto_code_".$strShippingCode);
+			$products =	$this->model_sale_order->getOrderProducts($order_id);
+			$index = 0;
+			foreach($products as $item) {				
+				//order for options			
+				$sku = $this->model_extension_nettivarasto->getSKUByProductId($item['product_id']);
+
+				$order_product_id = $this->model_extension_nettivarasto->getOrderProductIdByOrderId($strOrderDetails['order_id']);
+				
+				$orderOptions = $this->model_extension_nettivarasto->getOrderOptionsByOrderId($strOrderDetails['order_id'], $order_product_id[0]['order_product_id']);
+				
+				if(isset($orderOptions)) {
+					$optionSku = $sku.'-'.$orderOptions[0]['value'];
+
+					$order->setOrderLineCode( $index, $optionSku);
+				} else {
+					$order->setOrderLineCode( $index, ($this->model_extension_nettivarasto->getSKUByProductId($item['product_id'])) );
+				}			
+				$order->setOrderLineQuantity( $index, ($item['quantity']));
+				$index++;
+			}
 			  
-			  $order->setPriceTotal($strOrderDetails['total']);
-			  $order->setCustomerName($strOrderDetails['shipping_firstname'].' '.$strOrderDetails['shipping_lastname']);
-			  $order->setCustomerAddress1($strOrderDetails['shipping_address_1']);
-			  $order->setCustomerAddress2($strOrderDetails['shipping_address_2']);
-			  $order->setCustomerCity($strOrderDetails['shipping_city']);
-			  $order->setCustomerCountry($strOrderDetails['shipping_country']);
-			  $order->setCustomerEmail($strOrderDetails['email']);
-			  $order->setCustomerPhone($strOrderDetails['telephone']);
-			  $order->setCustomerZip($strOrderDetails['shipping_postcode']);
-			  $order->setShipping($nettivarasto_shipping_method);
-			  if ( $order->save() ) {
-				  $data['success'] = 'Order successfully transferred to Nettivarasto.';
-			  }
-			  else {
-				  $data['error_warning'] = 'Error - Nettivarasto API'. $this->api->getLastError();
-			  }   	
-		  }else{
-			 $data['error_warning'] = "Order Shipping method not enabled in settings";
-		  }
-	  return $data;
+			$order->setPriceTotal($strOrderDetails['total']);
+			$order->setCustomerName($strOrderDetails['shipping_firstname'].' '.$strOrderDetails['shipping_lastname']);
+			$order->setCustomerAddress1($strOrderDetails['shipping_address_1']);
+			$order->setCustomerAddress2($strOrderDetails['shipping_address_2']);
+			$order->setCustomerCity($strOrderDetails['shipping_city']);
+			$order->setCustomerCountry($strOrderDetails['shipping_country']);
+			$order->setCustomerEmail($strOrderDetails['email']);
+			$order->setCustomerPhone($strOrderDetails['telephone']);
+			$order->setCustomerZip($strOrderDetails['shipping_postcode']);
+			$order->setShipping($nettivarasto_shipping_method);
+
+			if ( $order->save() ) {
+				$data['success'] = 'Order successfully transferred to Nettivarasto.';
+			} else {
+				$data['error_warning'] = 'Error - Nettivarasto API'. $this->api->getLastError();
+			}   	
+		} else {
+			$data['error_warning'] = "Order Shipping method not enabled in settings";
+		}
+		return $data;
 	}
+	
 	protected function validate() {
 		if (!$this->user->hasPermission('modify', 'module/nettivarasto')) {
 			$this->error['warning'] = $this->language->get('error_permission');
--- upload/admin/model/extension/nettivarasto.php
+++ upload/admin/model/extension/nettivarasto.php
@@ -3,24 +3,87 @@ class ModelExtensionNettivarasto extends Model {
 	public function addHistory($order_id, $order_status_id, $comment) {
 		$his_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_history` WHERE order_id = '" . (int)$order_id . "' AND order_status_id = '" . (int)$order_status_id . "'");
 		if ($his_query->num_rows==0) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int)$order_id . "', order_status_id = '" . (int)$order_status_id . "', notify = '0', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
+			$this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int)$order_id . "', order_status_id = '" . (int)$order_status_id . "', notify = '0', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
 		}
 	}
+	
 	public function updateOrderStatus($order_id, $order_status_id) {
 		$this->db->query("UPDATE " . DB_PREFIX . "order SET order_status_id = '" . (int)$order_status_id . "' WHERE order_id='" . (int)$order_id . "'");
 	}
+	
 	public function getProductBySKU($sku) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product WHERE sku='".$sku."'");
 		return $query->row;
 	}
+	
 	public function getSKUByProductId($sku) {
 		$query = $this->db->query("SELECT sku FROM " . DB_PREFIX . "product WHERE product_id='".$sku."'");
 		return $query->row['sku'];
 	}
+	
 	public function updateProductQuantity($product_id, $quantity) {
 		$this->db->query("UPDATE " . DB_PREFIX . "product SET quantity = '" . (int)$quantity . "' WHERE product_id='" . (int)$product_id . "'");
 	}
+	
 	public function updateProductStockStatus($product_id, $stock) {
 		$this->db->query("UPDATE " . DB_PREFIX . "product SET stock_status_id = '" . (int)$stock . "' WHERE product_id='" . (int)$product_id . "'");
+	}
+	
+	// get product options with values
+	public function getProductOptionsWithValues($product_id) {
+		
+		$product_option_data = array();
+		
+		$sql = "SELECT * FROM " . DB_PREFIX . "product_option po LEFT JOIN `" . DB_PREFIX . "option` o ON (po.option_id = o.option_id) LEFT JOIN " . DB_PREFIX . "option_description od ON (o.option_id = od.option_id) WHERE po.product_id = '" . (int)$product_id . "' AND od.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY o.sort_order";
+		$product_option_query = $this->db->query($sql);
+
+		foreach ($product_option_query->rows as $product_option) {
+			$product_option_value_data = array();
+			
+			$optionSql = "SELECT * FROM " . DB_PREFIX . "product_option_value pov LEFT JOIN " . DB_PREFIX . "option_value ov ON (pov.option_value_id = ov.option_value_id) LEFT JOIN " . DB_PREFIX . "option_value_description ovd ON (ov.option_value_id = ovd.option_value_id) WHERE pov.product_id = '" . (int)$product_id . "' AND pov.product_option_id = '" . (int)$product_option['product_option_id'] . "' AND ovd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY ov.sort_order";
+			$product_option_value_query = $this->db->query($optionSql);
+
+			foreach ($product_option_value_query->rows as $product_option_value) {
+				$product_option_value_data[] = array(
+					'product_option_value_id' => $product_option_value['product_option_value_id'],
+					'option_value_id'         => $product_option_value['option_value_id'],
+					'name'                    => $product_option_value['name'],
+					'image'                   => $product_option_value['image'],
+					'quantity'                => $product_option_value['quantity'],
+					'subtract'                => $product_option_value['subtract'],
+					'price'                   => $product_option_value['price'],
+					'price_prefix'            => $product_option_value['price_prefix'],
+					'weight'                  => $product_option_value['weight'],
+					'weight_prefix'           => $product_option_value['weight_prefix']
+				);
+			}
+
+			$product_option_data[] = array(
+				'product_option_id'    => $product_option['product_option_id'],
+				'product_option_value' => $product_option_value_data,
+				'option_id'            => $product_option['option_id'],
+				'name'                 => $product_option['name'],
+				'type'                 => $product_option['type'],
+				'value'                => $product_option['value'],
+				'required'             => $product_option['required']
+			);
+		}
+
+		return $product_option_data;
+	}
+	
+	public function getOrderProductIdByOrderId($order_id) {
+		$sql = "SELECT order_product_id FROM " . DB_PREFIX . "order_product WHERE order_id = '" . (int)$order_id . "'";
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+		
+	}
+	
+	public function getOrderOptionsByOrderId($order_id, $order_product_id) {
+		$sql = "SELECT value FROM " . DB_PREFIX . "order_option WHERE order_id = '" . (int)$order_id . "' AND order_product_id = '" . (int)$order_product_id . "'";
+		$query = $this->db->query($sql);
+
+		return $query->rows;
 	}
 }
